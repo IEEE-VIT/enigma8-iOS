7aacf4d88e006846e9b99701dd6a67211f577784
Aaryan Kothari <aaryan.kothari@gmail.com>
2021-09-07 23:15:51 +0530

    Initial Commit

---
+++ Shared/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Shared
+//
+//  Created by Aaryan Kothari on 07/09/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Shared/EnigmaApp.swift
@@ -0,0 +1,17 @@
+//
+//  EnigmaApp.swift
+//  Shared
+//
+//  Created by Aaryan Kothari on 07/09/21.
+//
+
+import SwiftUI
+
+@main
+struct EnigmaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
