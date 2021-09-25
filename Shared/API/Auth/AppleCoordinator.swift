937f0135047f5cfe1c3418d1bfb592293b8a9ecd
Alok <alokme123@gmail.com>
2021-09-25 19:03:45 +0530

    fix: changes

--- Shared/API/Auth/AppleCoordinator.swift
+++ Shared/API/Auth/AppleCoordinator.swift
@@ -11,14 +11,9 @@ import CryptoKit
 
 class AppleCoordinator: NSObject, ASAuthorizationControllerDelegate {
     
-    private var currentNonce: String?
-    
     func signInWithApple() {
-        let nonce = randomNonceString()
-        currentNonce = nonce
         let provider = ASAuthorizationAppleIDProvider()
         let request = provider.createRequest()
-        request.nonce = sha256(nonce)
         request.requestedScopes = [.email,.fullName]
         let controller = ASAuthorizationController(authorizationRequests: [request])
         controller.performRequests()
@@ -27,9 +22,6 @@ class AppleCoordinator: NSObject, ASAuthorizationControllerDelegate {
     func authorizationController(controller: ASAuthorizationController, didCompleteWithAuthorization authorization: ASAuthorization) {
         switch authorization.credential {
         case let appleIDCredential as ASAuthorizationAppleIDCredential:
-            guard currentNonce != nil else {
-                fatalError("Invalid state: A login callback was received, but no login request was sent.")
-            }
             guard let appleIDToken = appleIDCredential.identityToken else {
                 print("Unable to fetch Identity Token")
                 return
@@ -38,8 +30,14 @@ class AppleCoordinator: NSObject, ASAuthorizationControllerDelegate {
                 print("Unable to serialize token string from data: \(appleIDToken.debugDescription)")
                 return
             }
-            guard let data = appleIDCredential.authorizationCode else {return}
-            guard let code = String(data: data, encoding: .utf8) else {return}
+            guard let data = appleIDCredential.authorizationCode else {
+                print("Unable to fetch Authorization Code")
+                return
+            }
+            guard let code = String(data: data, encoding: .utf8) else {
+                print("Unable to Serialize Authorization Code")
+                return
+            }
             
             //TODO: Send Code to backend and receive Authorized Token in AuthVM
         default:
@@ -49,49 +47,6 @@ class AppleCoordinator: NSObject, ASAuthorizationControllerDelegate {
     
     func authorizationController(controller: ASAuthorizationController, didCompleteWithError error: Error) {
         print(error.localizedDescription)
-    }
-    
-    // Adapted from https://auth0.com/docs/api-auth/tutorials/nonce#generate-a-cryptographically-random-nonce
-    private func randomNonceString(length: Int = 32) -> String {
-        precondition(length > 0)
-        let charset: Array<Character> =
-        Array("0123456789ABCDEFGHIJKLMNOPQRSTUVXYZabcdefghijklmnopqrstuvwxyz-._")
-        var result = ""
-        var remainingLength = length
-        
-        while remainingLength > 0 {
-            let randoms: [UInt8] = (0 ..< 16).map { _ in
-                var random: UInt8 = 0
-                let errorCode = SecRandomCopyBytes(kSecRandomDefault, 1, &random)
-                if errorCode != errSecSuccess {
-                    fatalError("Unable to generate nonce. SecRandomCopyBytes failed with OSStatus \(errorCode)")
-                }
-                return random
-            }
-            
-            randoms.forEach { random in
-                if length == 0 {
-                    return
-                }
-                
-                if random < charset.count {
-                    result.append(charset[Int(random)])
-                    remainingLength -= 1
-                }
-            }
-        }
-        
-        return result
-    }
-    
-    private func sha256(_ input: String) -> String {
-        let inputData = Data(input.utf8)
-        let hashedData = SHA256.hash(data: inputData)
-        let hashString = hashedData.compactMap {
-            return String(format: "%02x", $0)
-        }.joined()
-        
-        return hashString
     }
     
 }
