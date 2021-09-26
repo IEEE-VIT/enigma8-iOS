9a675a28e05150d2fa76f9687dbc88b19ba351f5
Ananya Elizabeth George <50709725+ananya190@users.noreply.github.com>
2021-09-26 20:24:47 +0530

    add: APIClient file

---
+++ Shared/API/APIClient.swift
@@ -0,0 +1,37 @@
+//
+//  APIClient.swift
+//  Enigma (iOS)
+//
+//  Created by Ananya George on 9/26/21.
+//
+
+import Foundation
+import Alamofire
+
+class APIClient {
+    
+    static let sessionManager: Session = {
+        let config = URLSessionConfiguration.default
+        config.timeoutIntervalForRequest = 20
+        let session = Session(configuration: config)
+        return session
+    }()
+    
+    class func request<T: Codable>(fromRouter router: Router, callback: @escaping (_ response: T?, _ error: String?) -> Void) {
+        APIClient.sessionManager.request(router).responseDecodable { (response: DataResponse<T, AFError>) in
+            switch response.result {
+            case .success(let obj):
+                print("SUCCESS: \(obj)")
+                callback(obj,nil)
+            case .failure(let error):
+                print("FAILURE2")
+                print(error)
+            }
+            
+            print(response.request?.url?.absoluteString ?? "")
+            print(response.request?.headers ?? "")
+            print(try? response.request?.httpBody?.toJSON())
+        }
+    }
+    
+}
