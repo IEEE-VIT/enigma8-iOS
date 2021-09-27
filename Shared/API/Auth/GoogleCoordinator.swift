d824cc7b6b615edc40acab81da1ddc659077e0f7
Shriram <shrirama786@gmail.com>
2021-09-27 14:39:21 +0530

    issue resolved

--- Shared/API/Auth/GoogleCoordinator.swift
+++ Shared/API/Auth/GoogleCoordinator.swift
@@ -11,26 +11,22 @@ import GoogleSignIn
 // MARK: - SIGNIN WITH GOOGLE COORDINATOR
 
 class GoogleCoordinator: NSObject {
-    
-   
     var authVM: AuthViewModel
     let signInConfig: GIDConfiguration
-    
     init(authVM: AuthViewModel) {
         self.authVM = authVM
-        self.signInConfig = GIDConfiguration.init(clientID: "to do", serverClientID: "to do")
+        self.signInConfig = GIDConfiguration.init(clientID: AppConstants.Google.clientId, serverClientID: AppConstants.Google.serverClientId)
         super.init()
     }
-   
-    
-    // google sign in flow
+   // google sign in flow
     func googleSignin() {
-        GIDSignIn.sharedInstance.signIn(with: signInConfig, presenting: (UIApplication.shared.windows.last?.rootViewController)!) { user, error in
-            guard error == nil,user != nil else {
+        let vc = (UIApplication.shared.windows.last?.rootViewController)!
+        GIDSignIn.sharedInstance.signIn(with: signInConfig, presenting: vc) { user, error in
+            guard let error = error else {
                 print(error?.localizedDescription ?? "")
                 return
             }
-      // TODO: self.authVM.signinWithGoogle(with: user?.serverAuthCode ?? "")
+         self.authVM.signInWithGoogle(with: user?.serverAuthCode ?? "")
         }
     }
     
