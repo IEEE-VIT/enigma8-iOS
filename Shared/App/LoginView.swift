3751f457b64f67e50325c4bddfbe3bb300009366
Ananya Elizabeth George <50709725+ananya190@users.noreply.github.com>
2021-10-05 16:38:57 +0530

    create: LoginView.swift

---
+++ Shared/App/LoginView.swift
@@ -0,0 +1,23 @@
+//
+//  LoginView.swift
+//  Enigma (iOS)
+//
+//  Created by Ananya George on 10/5/21.
+//
+
+import SwiftUI
+
+struct LoginView: View {
+    var body: some View {
+        VStack {
+            Text("ENIGMA")
+                .font(.title)
+        }
+    }
+}
+
+struct LoginView_Previews: PreviewProvider {
+    static var previews: some View {
+        LoginView()
+    }
+}
