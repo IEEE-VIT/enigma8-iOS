eacb24f0ed80e3db5eccfd3630afa3941ff109a0
Ananya Elizabeth George <50709725+ananya190@users.noreply.github.com>
2021-10-16 22:48:34 +0530

    feat: profile setup ui (#48)
    * feat: profile setup ui
    
    * feat: routes for user setup
    
    * fix: stop view from animating when dropdown is tapped
    
    * feat: add profile setup view model
    
    * fix: profilesetupview button

---
+++ Shared/Views/Common/CustomTextField.swift
@@ -0,0 +1,19 @@
+//
+//  CustomTextField.swift
+//  Enigma (iOS)
+//
+//  Created by Ananya George on 10/14/21.
+//
+
+import SwiftUI
+
+struct CustomTextField: View {
+    var textFieldString: String
+    var bindingString: Binding<String>
+    var body: some View {
+       TextField(textFieldString, text: bindingString)
+            .textFieldStyle(RoundedBorderTextFieldStyle())
+            .padding(.top)
+        
+    }
+}
