058769979a1b050c8f5755808e79d043916c961d
Ananya Elizabeth George <50709725+ananya190@users.noreply.github.com>
2021-10-18 11:33:55 +0530

    add: scrollview

--- Shared/App/RoomsView.swift
+++ Shared/App/RoomsView.swift
@@ -6,74 +6,57 @@
 //
 
 import SwiftUI
+import Kingfisher
 
 struct RoomsView: View {
-    var timerVM: TimerViewModel = TimerViewModel()
-    var enigmaEnd: Date {
-        Date(timeInterval: 172800,since: timerVM.enigmaDate ?? Date())
-    }
-    var timeLeft: DateComponents {
-        Calendar.current.dateComponents([.hour,.minute,.second], from: Date(), to: enigmaEnd)
-    }
-    var numberOfStars: Int
-    let rooms = [0,2,4,6]
     let columns = [
         GridItem(.flexible()),
         GridItem(.flexible())
     ]
+    var rooms: [RoomModel]
+    
     var body: some View {
-        VStack {
-            // MARK: TITLE
-            HStack {
-                Text("Rooms")
-                    .font(.title)
+        ScrollView {
+            VStack {
+                // MARK: TITLE
+                HStack {
+                    Text("Rooms")
+                        .font(.title)
+                    Spacer()
+                }
+                
                 Spacer()
-                Label(": \(numberOfStars)", systemImage: "star.fill")
-                    .font(.title)
-            }
-            // MARK: TIMER
-            HStack {
-                TimerBlockView(value: (timeLeft.hour ?? 0) / 10)
-                TimerBlockView(value: (timeLeft.hour ?? 0 ) % 10)
-                Text(":")
-                TimerBlockView(value: (timeLeft.minute ?? 0) / 10)
-                TimerBlockView(value: (timeLeft.minute ?? 0 ) % 10)
-                Text(":")
-                TimerBlockView(value: (timeLeft.second ?? 0) / 10)
-                TimerBlockView(value: (timeLeft.second ?? 0 ) % 10)
-            }
-            Spacer()
-            // MARK: GRID
-            ForEach(rooms, id: \.self) { roomNum in
-                HStack() {
-                    ForEach(roomNum+1 ..< roomNum+3) { rNumber in
-                        Spacer()
+                // MARK: GRID
+                LazyVGrid(columns: columns, spacing: 20) {
+                    ForEach(rooms) { room in
                         VStack {
-                            Spacer()
-                            Image(systemName: "star")
-                            Text("Room \(rNumber)")
-                            Spacer()
+                            HStack {
+                                ForEach(0..<3) { _ in
+                                    Image(systemName: "circle")
+                                }
+                            }
+                            KFImage(URL(string: room.media ?? ""))
+                                .resizable()
+                                .scaledToFit()
+                            Text(room.title ?? "Room \(room.roomNo ?? 0)")
                         }
-                        Spacer()
                     }
+//                    ForEach(rooms, id: \.self) { roomnum in
+//                        VStack {
+//                            Text("Room \(roomnum)")
+//                            Image(systemName: "star")
+//                                .resizable()
+//                        }
+//                    }
                 }
             }
-//            LazyVGrid(columns: columns, spacing: 20) {
-//                ForEach(rooms, id: \.self) { roomnum in
-//                    VStack {
-//                        Text("Room \(roomnum)")
-//                        Image(systemName: "star")
-//                            .resizable()
-//                    }
-//                }
-//            }
+            .padding()
         }
-        .padding()
     }
 }
 
 struct RoomsView_Previews: PreviewProvider {
     static var previews: some View {
-        RoomsView(numberOfStars: 2)
+        RoomsView(rooms: RoomModel.sampleData)
     }
 }
