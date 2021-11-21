c8b4036f29a5d99a11115b9ebd4f3278b3060ef3
Aaryan Kothari <53724307+aaryankotharii@users.noreply.github.com>
2021-11-21 13:08:52 +0530

    FEEDBACK (#130)

---
+++ Shared/Model/FeedbackModel.swift
@@ -0,0 +1,32 @@
+//
+//  FeedbackModel.swift
+//  Enigma (iOS)
+//
+//  Created by Aaryan Kothari on 21/11/21.
+//
+
+import Foundation
+
+class FeedbackModel: Codable {
+    var isVITStudent: Bool
+    var regNo: String?
+    var vitEmail: String?
+    var gameRating: Int
+    var userExperience: String
+    var featureIdeas: String
+    
+    internal init(isVITStudent: Bool, regNo: String? = nil, vitEmail: String? = nil, gameRating: Int, userExperience: String, featureIdeas: String) {
+        self.isVITStudent = isVITStudent
+        self.regNo = regNo
+        self.vitEmail = vitEmail
+        self.gameRating = gameRating
+        self.userExperience = userExperience
+        self.featureIdeas = featureIdeas
+    }
+}
+
+
+
+struct FeedbackFilled: Codable {
+    var feedbackFilled: Bool?
+}
