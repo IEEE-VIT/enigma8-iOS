ff371b5a29ecb5461f968543de8fe5e744a8fb8d
Alok <alokme123@gmail.com>
2021-11-23 11:05:01 +0530

    fix: key in instructionsview twice

--- Shared/Views/Instructions/GameMechanics.swift
+++ Shared/Views/Instructions/GameMechanics.swift
@@ -13,45 +13,34 @@ struct GameMechanics: View {
         
         VStack(alignment: .center, spacing: 20) {
             ScrollView(showsIndicators:false) {
-            VStack(alignment: .leading) {
-                ForEach(data, id: \.self) { point in
-                    HStack(alignment: .top) {
-                        Image("BulletPoint")
+                VStack(alignment: .leading) {
+                    ForEach(data, id: \.self) { point in
+                        HStack(alignment: .top) {
+                            Image("BulletPoint")
+                                .resizable()
+                                .scaledToFit()
+                                .frame(width: 8)
+                                .padding(.top, 5)
+                            Text(point)
+                                .font(.Mulish(size: 18, weight: .medium))
+                                .foregroundColor(Color.eGold)
+                        }
+                    }
+                    HStack {
+                        Spacer()
+                        Image("Key")
                             .resizable()
                             .scaledToFit()
-                            .frame(width: 8)
-                            .padding(.top, 5)
-                        Text(point)
-                            .font(.Mulish(size: 18, weight: .medium))
-                            .foregroundColor(Color.eGold)
+                            .frame(height: 35)
+                        Text(": Key")
+                            .font(.Mulish(size: 14, weight: .regular))
+                            .foregroundColor(Color.eBlue)
+                        Spacer()
                     }
+                    .padding()
+                    Spacer()
                 }
-                Spacer()
-                HStack {
-                    Image("Key")
-                        .resizable()
-                        .scaledToFit()
-                        .frame(height: 35)
-                    Text(": Key")
-                        .font(.Mulish(size: 14, weight: .regular))
-                        .foregroundColor(Color.eBlue)
-                }
-                .padding()
-                Spacer()
             }
-            }
-            Spacer()
-            HStack {
-                Image("Key")
-                    .resizable()
-                    .scaledToFit()
-                    .frame(height: 35)
-                Text(": Key")
-                    .font(.Mulish(size: 14, weight: .regular))
-                    .foregroundColor(Color.eBlue)
-            }
-            .padding()
-            Spacer()
         }
         .padding()
     }
